5a4cd0a74ae30ec953a37c5e1733b425da76621b
Rachel Kurchin <rkurchin@cmu.edu>
2020-07-29 17:40:32 -0400

    update Cu DOS, working on dataviz nb

--- export_figure_data.jl
+++ export_figure_data.jl
@@ -4,8 +4,7 @@ stepsize=0.005
 
 ## FIGURE 1
 # Li100 interpolated DOS
-dosfile = "Li_100_dos_10smooth.txt"
-#dosfile = "DOSes/Li_100_dos.txt"
+dosfile = "DOSes/Li_100_dos.txt"
 dos_f, avg_dos, E_min, E_max = get_dos(dosfile)
 E_vals = range(E_min, E_max, step=stepsize)
 dos_vals = dos_f.(E_vals)
@@ -40,7 +39,7 @@ end;
 
 ## FIGURE 2
 # Cu111 DOS
-dos_f, avg_dos, E_min, E_max = get_dos("DOSes/Cu111dos.txt")
+dos_f, avg_dos, E_min, E_max = get_dos("DOSes/Cu_111_dos.txt")
 E_vals = range(E_min, E_max, step=stepsize)
 dos_vals = dos_f.(E_vals)
 dos_data = hcat(E_vals, dos_vals)
@@ -61,7 +60,7 @@ end;
 
 ## FIGURE 3
 # SEI (semiconductor) case
-dos_f, avg_dos, E_min, E_max = get_dos("DOSes/LiF_Li2CO3dos.txt")
+dos_f, avg_dos, E_min, E_max = get_dos("DOSes/LiF_Li2CO3_dos.txt")
 E_vals = range(E_min, E_max, step=stepsize)
 dos_vals = dos_f.(E_vals)
 dos_data = hcat(E_vals, dos_vals)
@@ -83,7 +82,7 @@ end;
 ## FIGURE 4
 # impact of changing eq. Ef on SEI
 # first do DOS again
-dos_f, avg_dos, E_min, E_max = get_dos("DOSes/LiF_Li2CO3dos.txt"; Ef=-2.0)
+dos_f, avg_dos, E_min, E_max = get_dos("DOSes/LiF_Li2CO3_dos.txt"; Ef=-2.0)
 E_vals = range(E_min, E_max, step=stepsize)
 dos_vals = dos_f.(E_vals)
 dos_data = hcat(E_vals, dos_vals)
@@ -102,7 +101,7 @@ end;
 
 
 # and now, impact of changing λ on the Cu111 case
-dos_f, avg_dos, E_min, E_max = get_dos("DOSes/Cu111dos.txt")
+dos_f, avg_dos, E_min, E_max = get_dos("DOSes/Cu_111_dos.txt")
 V_range = range(-5.0, 5.0, step=stepsize)
 MHC_k = [compute_k_MHC(E_min, E_max, MHC_DOS_λ*2, V, avg_dos) for V in V_range]
 MHC_DOS_k = [compute_k_MHC_DOS(E_min, E_max, MHC_DOS_λ*2, V, dos_f) for V in V_range]

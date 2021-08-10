5484ecb4155c7dd0ea0662f7737eab23305eacd0
Holden Parks <holdenparks@yahoo.com>
2021-08-10 12:27:19 -0400

    changed overpotential to V_dl in red/ox rate calculation functions. Removed #TODO flag. Made example in calculate_k_cq.jl simpler.

--- src/analysis_fcns.jl
+++ src/analysis_fcns.jl
@@ -49,8 +49,8 @@ function MHC_integrand(E, λ, V_dl, average_dos; kT=.026)
 end
 
 
-function compute_k_MHC(E_min, E_max, λ, eη, average_dos; kT=.026)
-    fcn = E->MHC_integrand(E, λ, eη, average_dos; kT=.026)
+function compute_k_MHC(E_min, E_max, λ, V_dl, average_dos; kT=.026)
+    fcn = E->MHC_integrand(E, λ, V_dl, average_dos; kT=.026)
     quadgk(fcn, E_min, E_max)[1] # return format is (value, error_bound)
 end
 
@@ -81,6 +81,7 @@ function calculate_Vdl_interp(dos_f, Vq_min, Vq_max, C_dl)
 end
 
 function compute_k_MHC_DOS(E_min, E_max, λ, V_app, dos_func; kT=.026, C_q_calc=false, C_dl=10.0, Vq_min=-0.5, Vq_max=0.5)
+    
     if C_q_calc
         ## calculate V_dl from V_app by calculating CQ
         V_dl_interp = calculate_Vdl_interp(dos_func, Vq_min, Vq_max, C_dl)
@@ -93,24 +94,32 @@ function compute_k_MHC_DOS(E_min, E_max, λ, V_app, dos_func; kT=.026, C_q_calc=
     quadgk(fcn, E_min, E_max)[1]
 end
 
-function MHC_DOS_integrand_redox(E, λ, eη, dos_func; ox=true, kT=.026, vq=0)
-    dos_func(E+vq) * MHC_integrand_redox(E, λ, eη, 1; ox, kT=kT)
+function MHC_DOS_integrand_redox(E, λ, V_dl, dos_func; ox=true, kT=.026, V_q=0)
+    dos_func(E+V_q) * MHC_integrand_redox(E, λ, V_dl, 1; ox, kT=kT)
 end
 
-function MHC_integrand_redox(E, λ, eη, average_dos; ox=true, kT=.026)
+function MHC_integrand_redox(E, λ, V_dl, average_dos; ox=true, kT=.026)
     if ox
-        marcus = marcus_integrand(E, λ, eη, true; kT=kT)
+        marcus = marcus_integrand(E, λ, V_dl, true; kT=kT)
         fd = 1-fermi_dirac(E; kT=kT) # holes
     else
-        marcus = marcus_integrand(E, λ, eη, false; kT=kT)
+        marcus = marcus_integrand(E, λ, V_dl, false; kT=kT)
         fd = fermi_dirac(E; kT=kT)
     end
     average_dos * marcus * fd
 end
 
 
-function compute_k_MHC_DOS_redox(E_min, E_max, λ, eη, dos_func; ox=true, kT=.026, vq=0)
-    fcn = E->MHC_DOS_integrand_redox(E, λ, eη, dos_func; ox=ox, kT=kT, vq=vq)
+function compute_k_MHC_DOS_redox(E_min, E_max, λ, V_app, dos_func; ox=true, kT=.026, C_q_calc=false, C_dl=10.0, Vq_min=-0.5, Vq_max=0.5)
+    
+    if C_q_calc
+        V_dl_interp = calculate_Vdl_interp(dos_func, Vq_min, Vq_max, C_dl)
+        V_dl = V_dl_interp(V_app)
+        V_q = V_app - V_dl
+        fcn = E->MHC_DOS_integrand_redox(E, λ, V_dl, dos_func; ox=ox, kT=kT, V_q=V_q)
+    else
+        fcn = E->MHC_DOS_integrand_redox(E, λ, V_dl, dos_func; ox=ox, kT=kT, V_q=0)
+    end
     quadgk(fcn, E_min, E_max)[1]
 end
 
@@ -118,7 +127,6 @@ function QC_integrand(E, eVq, dos_func; kT=.026)
     dos_func(E) * sech((E-eVq)/(2*kT))^2/(4*kT)
 end
 
-## TODO: reformat so that qc is calculated entirely internally
 function compute_cq(E_min, E_max, eVq, dos_func; kT=.026)
     fcn = E->QC_integrand(E, eVq, dos_func; kT=kT)
     quadgk(fcn, E_min, E_max)[1]

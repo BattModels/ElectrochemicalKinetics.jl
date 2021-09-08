af0906f2bbf0185c644b0c1454c8aa9db1d99a25
Rachel Kurchin <rkurchin@cmu.edu>
2021-09-08 14:02:11 -0400

    start adding docstrings
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- src/analysis_fcns.jl
+++ src/analysis_fcns.jl
@@ -6,14 +6,30 @@ using DelimitedFiles
 using BlackBoxOptim
 using Dierckx
 
-# assumes `dos_file` has two columns: energy (relative to equilibrium Ef) and DOS
-# NB that cut_energy will break if the upper bound is larger magnitude than lower as in some of the lithium cases
+"""
+    fermi_dirac(E; kT=0.026)
+
+Compute the value of the Fermi-Dirac distribution at energy `E` (relative to the Fermi energy) and thermal energy `kT`.
+"""
+fermi_dirac(E; kT=.026) = 1 / (1 + exp(E/kT))
+
+"""
+    get_dos(dos_file; Ef=0, cut_energy=false)
+
+Retrieve density of states data from a file at path `dos_file`, assuming that the Fermi level is at energy `Ef`.
+
+Returns a callable interpolated DOS, the average value of DOS, and the lower and upper bound energies.
+
+# Notes
+* `dos_file` is assumed to contain two colums, energy and DOS value
+* If `cut_energy==true`, return data in a symmetric range of energies around `Ef` (this is sometimes useful for nice-looking plots). Note that the code presumes the cutoff for this comes from the upper bound. This would not be hard to fix, I just have not done so as yet.
+"""
 function get_dos(dos_file; Ef=0, cut_energy=false)
     dos_data = readdlm(dos_file, Float64, skipstart=1)
     # recenter so Ef=0
     dos_data[:,1] = dos_data[:,1] .- Ef
     E_max = dos_data[end,1]
-    if cut_energy
+    if cut_energy #TODO: fix this so that it works if magnitude of upper bound is smaller
         # for now, we'll pick a symmetric energy range about 0 (i.e. the new Ef)
         len_keep = sum(dos_data[:,1] .> -E_max)
         # cut off
@@ -27,10 +43,22 @@ function get_dos(dos_file; Ef=0, cut_energy=false)
     return dos_interp, average_dos, E_min, E_max
 end
 
-function fermi_dirac(E; kT=.026)
-    1 / (1 + exp(E/kT))
-end
+"""
+    marcus_integrand(E, λ, V_dl, ox=true; kT=.026)
 
+Compute the value of the Marcus integrand, given by:
+``-(λ - V_{dl} + E)^2 / (4 λ kT)``
+in the oxidative direction (when `ox==true`), and
+``-(λ + V_{dl} - E)^2 / (4 λ kT)``
+in the reductive direction (when `ox==false`)
+
+# Arguments (all energies are in eV)
+* `E`: energy coordinate at which to evaluate integrand
+* `λ`: reorganization energy parameter
+* `V_dl`: voltage at the interface (equal to applied voltage in absence of quantum capacitance, see [`calculate_Vdl_interp`](@ref))
+* `ox`: boolean flag for oxidative vs. reductive direction of reaction
+* `kT`: thermal energy, defaults to room temperature value
+"""
 function marcus_integrand(E, λ, V_dl, ox=true; kT=.026)
     if ox # oxidative direction
         arg = -(λ-V_dl+E)^2 / (4*λ*kT)
@@ -40,7 +68,27 @@ function marcus_integrand(E, λ, V_dl, ox=true; kT=.026)
     exp(arg)
 end
 
-function MHC_integrand(E, λ, V_dl, average_dos; kT=.026)
+"""
+    MHC_integrand(E, λ, V_dl, average_dos, ox=true; kT=.026)
+
+Compute the value of the Marcus-Hush-Chidsey integrand, given by the value of the Marcus integrand (see [`marcus_integrand`](@ref)) multiplied by the appropriate Fermi-Dirac distribution (for electrons or holes).
+
+# Arguments (all energies are in eV)
+* `E`: energy coordinate at which to evaluate integrand
+* `λ`: reorganization energy parameter
+* `V_dl`: voltage at the interface (equal to applied voltage in absence of quantum capacitance, see [`calculate_Vdl_interp`](@ref))
+* `average_dos`: average value of the density of states (this is merely a scaling factor, useful for direct comparisons to the results of the full MHC+DOS method)
+* `ox`: boolean flag for oxidative vs. reductive direction of reaction
+* `kT`: thermal energy, defaults to room temperature value
+"""
+function MHC_integrand(E, λ, V_dl, average_dos; ox=true, kT=.026)
+    marcus = marcus_integrand(E, λ, V_dl, ox; kT=kT)
+    fd = ox ? 1-fermi_dirac(E; kT=kT) : fermi_dirac(E; kT=kT)
+    average_dos * marcus * fd
+end
+
+# TODO: rewrite this in terms of ox and red fcns
+function MHC_integrand_net(E, λ, V_dl, average_dos; kT=.026)
     marcus_ox = marcus_integrand(E, λ, V_dl, true; kT=kT)
     marcus_red = marcus_integrand(E, λ, V_dl, false; kT=kT)
     fd_ox = 1-fermi_dirac(E; kT=kT) # holes
@@ -50,18 +98,17 @@ end
 
 
 function compute_k_MHC(E_min, E_max, λ, V_dl, average_dos; kT=.026)
-    fcn = E->MHC_integrand(E, λ, V_dl, average_dos; kT=.026)
+    fcn = E -> MHC_integrand_net(E, λ, V_dl, average_dos; kT=.026)
     quadgk(fcn, E_min, E_max)[1] # return format is (value, error_bound)
 end
 
 # compute with DOS by evaluating the function we already have for a unit DOS and multiplying
 # by the interpolated value
 function MHC_DOS_integrand(E, λ, V_dl, dos_func; kT=.026, V_q=0)
-    dos_func(E+V_q) * MHC_integrand(E, λ, V_dl, 1; kT=kT)
+    dos_func(E+V_q) * MHC_integrand_net(E, λ, V_dl, 1; kT=kT)
 end
 
 function calculate_Vdl_interp(dos_f, Vq_min, Vq_max, C_dl)
-    
     Vq_range = range(Vq_min, Vq_max, step=0.001)
     E_min = dos_f.ranges[1][1]
     E_max = dos_f.ranges[1][end]
@@ -80,8 +127,8 @@ function calculate_Vdl_interp(dos_f, Vq_min, Vq_max, C_dl)
     return v_interp
 end
 
+# TODO: rewrite this in terms of redox fcns
 function compute_k_MHC_DOS(E_min, E_max, λ, V_app, dos_func; kT=.026, C_q_calc=false, C_dl=10.0, Vq_min=-0.5, Vq_max=0.5)
-    
     if C_q_calc
         ## calculate V_dl from V_app by calculating CQ
         V_dl_interp = calculate_Vdl_interp(dos_func, Vq_min, Vq_max, C_dl)
@@ -95,23 +142,13 @@ function compute_k_MHC_DOS(E_min, E_max, λ, V_app, dos_func; kT=.026, C_q_calc=
 end
 
 function MHC_DOS_integrand_redox(E, λ, V_dl, dos_func; ox=true, kT=.026, V_q=0)
-    dos_func(E+V_q) * MHC_integrand_redox(E, λ, V_dl, 1; ox, kT=kT)
+    dos_func(E+V_q) * MHC_integrand(E, λ, V_dl, 1 ox; kT=kT)
 end
 
-function MHC_integrand_redox(E, λ, V_dl, average_dos; ox=true, kT=.026)
-    if ox
-        marcus = marcus_integrand(E, λ, V_dl, true; kT=kT)
-        fd = 1-fermi_dirac(E; kT=kT) # holes
-    else
-        marcus = marcus_integrand(E, λ, V_dl, false; kT=kT)
-        fd = fermi_dirac(E; kT=kT)
-    end
-    average_dos * marcus * fd
-end
+
 
 
 function compute_k_MHC_DOS_redox(E_min, E_max, λ, V_app, dos_func; ox=true, kT=.026, C_q_calc=false, C_dl=10.0, Vq_min=-0.5, Vq_max=0.5)
-    
     if C_q_calc
         V_dl_interp = calculate_Vdl_interp(dos_func, Vq_min, Vq_max, C_dl)
         V_dl = V_dl_interp(V_app)

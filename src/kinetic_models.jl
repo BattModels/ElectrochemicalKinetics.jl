b3917d798c53b02fa01b4816efec2cac1e213d7f
Rachel Kurchin <rkurchin@cmu.edu>
2021-09-08 20:25:24 -0400

    more cleanup, not yet tested
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- src/kinetic_models.jl
+++ src/kinetic_models.jl
@@ -3,27 +3,26 @@
 
 Compute the value of the Fermi-Dirac distribution at energy `E` (relative to the Fermi energy) and thermal energy `kT`.
 """
-fermi_dirac(E, kT=.026) = 1 / (1 + exp(E/kT))
+fermi_dirac(E; kT = 0.026) = 1 / (1 + exp(E / kT))
 
-abstract type KineticModel end 
+abstract type KineticModel end
 
-# checks to catch missed dispatches for new types
-integrand(km::KineticModel, Reduction, V_app, direction, kT=.026) = error("A kinetic model must dispatch the `integrand` function in the reductive direction!")
-integrand(km::KineticModel, Oxidation, V_app, direction, kT=.026) = error("A kinetic model must dispatch the `integrand` function in the oxidative direction!")
+# check to catch missed dispatches for new types
+integrand(km::KineticModel, V_dl::Real, ox::Bool; kT::Real = 0.026) =
+    error("A kinetic model must dispatch the `integrand` function!")
 
 # dispatch for net rates
-integrand(km::KineticModel, V_dl, kT=.026) = abs(integrand(km, V_dl, true; kT=kT) - integrand(km, V_dl, false, kT))
+integrand(km::KineticModel, V_dl::Real; kT::Real  = 0.026) =
+    E -> abs(integrand(km, V_dl, true; kT = kT)(E) - integrand(km, V_dl, false; kT = kT)(E))
 
 struct Marcus <: KineticModel
     λ::Float64
 end
 
-function integrand(m::Marcus, V_dl<:Real, ox::Bool, kT=.026) 
-    if ox # oxidative direction
-        arg = E -> -(m.λ-V_dl+E)^2 / (4*m.λ*kT)
-    else # reductive direction
-        arg = E -> -(m.λ+V_dl-E)^2 / (4*m.λ*kT)
-    end
+function integrand(m::Marcus, V_dl::Real, ox::Bool; kT::Real  = 0.026)
+    arg =
+        E ->
+            ox ? -(m.λ - V_dl + E)^2 / (4 * m.λ * kT) : -(m.λ + V_dl - E)^2 / (4 * m.λ * kT)
     E -> exp(arg(E))
 end
 
@@ -32,10 +31,9 @@ struct MarcusHushChidsey <: KineticModel
     average_dos::Float64
 end
 
-function integrand(mhc::MarcusHushChidsey, V_dl<:Real, ox::Bool, kT=.026)
-    # TODO: check dispatch of Net and add assertion if necessary
-    marcus = E -> integrand(Marcus(mhc.λ), V_dl, ox, kT)
-    fd(E) = ox ? 1-fermi_dirac(E, kT) : fermi_dirac(E, kT)
+function integrand(mhc::MarcusHushChidsey, V_dl::Real, ox::Bool; kT::Real  = 0.026)
+    marcus = integrand(Marcus(mhc.λ), V_dl, ox; kT = kT)
+    fd(E) = ox ? 1 - fermi_dirac(E; kT = kT) : fermi_dirac(E; kT = kT)
     return E -> mhc.average_dos * marcus(E) * fd(E)
 end
 
@@ -44,87 +42,7 @@ struct MarcusHushChidseyDOS <: KineticModel
     dos::DOSData
 end
 
+integrand(mhcd::MarcusHushChidseyDOS, V_dl::Real, ox::Bool; kT::Real = .026, V_q=0.0) = E -> mhcd.dos.interp_func(E+V_q) * integrand(MarcusHushChidsey(mhcd.λ, mhcd.dos.average_value), V_dl, ox; kT=kT)(E)
 
-
-"""
-    marcus_integrand(E, λ, V_dl, ox=true; kT=.026)
-
-Compute the value of the Marcus integrand, given by:
-``-(λ - V_{dl} + E)^2 / (4 λ kT)``
-in the oxidative direction (when `ox==true`), and
-``-(λ + V_{dl} - E)^2 / (4 λ kT)``
-in the reductive direction (when `ox==false`)
-
-# Arguments (all energies are in eV)
-* `E`: energy coordinate at which to evaluate integrand
-* `λ`: reorganization energy parameter
-* `V_dl`: voltage at the interface (equal to applied voltage in absence of quantum capacitance, see [`calculate_Vdl_interp`](@ref))
-* `ox`: boolean flag for oxidative vs. reductive direction of reaction
-* `kT`: thermal energy, defaults to room temperature value
-"""
-function marcus_integrand(E, λ, V_dl, ox; kT=.026)
-    if ox # oxidative direction
-        arg = -(λ-V_dl+E)^2 / (4*λ*kT)
-    else # reductive direction
-        arg = -(λ+V_dl-E)^2 / (4*λ*kT)
-    end
-    exp(arg)
-end
-
-"""
-    MHC_integrand(E, λ, V_dl, average_dos, ox; kT=.026)
-    MHC_integrand(E, λ, V_dl, dd::DOSData, ox; kT=.026)
-
-Compute the value of the Marcus-Hush-Chidsey integrand, given by the value of the Marcus integrand (see [`marcus_integrand`](@ref)) multiplied by the appropriate Fermi-Dirac distribution (for electrons or holes).
-
-# Arguments (all energies are in eV)
-* `E`: energy coordinate at which to evaluate integrand
-* `λ`: reorganization energy parameter
-* `V_dl`: voltage at the interface (equal to applied voltage in absence of quantum capacitance, see [`calculate_Vdl_interp`](@ref))
-* `average_dos`: average value of the density of states (this is merely a scaling factor, useful for direct comparisons to the results of the full MHC+DOS method)
-* `ox`: boolean flag for oxidative vs. reductive direction of reaction
-* `kT`: thermal energy, defaults to room temperature value
-"""
-function MHC_integrand(E, λ, V_dl, average_dos, ox; kT=.026)
-    marcus = marcus_integrand(E, λ, V_dl, ox; kT=kT)
-    fd = ox ? 1-fermi_dirac(E; kT=kT) : fermi_dirac(E; kT=kT)
-    average_dos * marcus * fd
-end
-
-MHC_integrand(E, λ, V_dl, dd::DOSData, ox; kT=.026) = MHC_integrand(E, λ, V_dl, dd.average_value, ox; kT=kT)
-
-"""
-    MHC_integrand_net(E, λ, V_dl, average_dos; kT=.026)
-    MHC_integrand_net(E, λ, V_dl, dd::DOSData; kT=.026)
-
-Convenience function to compute the value of the net Marcus-Hush-Chidsey integrand (magnitude of difference between oxidative and reductive directions).
-
-See also: [`MHC_integrand`](@ref)
-"""
-MHC_integrand_net(E, λ, V_dl, average_dos; kT=.026) = sign(V_dl) * (MHC_integrand(E, λ, V_dl, average_dos, true) - MHC_integrand(E, λ, V_dl, average_dos, false))
-MHC_integrand_net(E, λ, V_dl, dd::DOSData; kT=.026) = MHC_integrand_net(E, λ, V_dl, dd.average_value; kT=kT)
-
-"""
-    MHC_DOS_integrand(E, λ, V_dl, dos_func, ox=true; kT=.026)
-
-Compute the value of the Marcus-Hush-Chidsey integrand, given by the value of the Marcus integrand (see [`marcus_integrand`](@ref)) multiplied by the appropriate Fermi-Dirac distribution (for electrons or holes).
-
-# Arguments (all energies are in eV)
-* `E`: energy coordinate at which to evaluate integrand
-* `λ`: reorganization energy parameter
-* `V_dl`: voltage at the interface (equal to applied voltage in absence of quantum capacitance, see [`calculate_Vdl_interp`](@ref))
-* `dos_func`: Callable object capable of evaluating the density of states at any value of energy within relevant bounds (can be produced from a data file using [`get_dos`](@ref))
-* `ox`: boolean flag for oxidative vs. reductive direction of reaction
-* `kT`: thermal energy, defaults to room temperature value
-* `V_q`: voltage offset due to quantum capacitance [HOLDEN TO CHECK THIS DESCRIPTION]
-"""
-MHC_DOS_integrand(E, λ, V_dl, dos_func, ox; kT=.026, V_q=0) = dos_func(E+V_q) * MHC_integrand(E, λ, V_dl, 1, ox; kT=kT)
-
-"""
-    MHC_integrand_net(E, λ, V_dl, dos_func; kT=.026, V_q=0)
-
-Convenience function to compute the value of the net Marcus-Hush-Chidsey + DOS integrand (magnitude of difference between oxidative and reductive directions).
-
-See also: [`MHC_DOS_integrand`](@ref)
-"""
-MHC_DOS_integrand_net(E, λ, V_dl, dos_func; kT=.026, V_q=0) = dos_func(E+V_q) * MHC_integrand_net(E, λ, V_dl, 1; kT=kT)
+# do our own net-rate dispatch here to pass through V_q, seems like there should be a more elegant solution
+integrand(mhcd::MarcusHushChidseyDOS, V_dl::Real; kT::Real = .026, V_q=0.0) = E -> mhcd.dos.interp_func(E+V_q) * integrand(MarcusHushChidsey(mhcd.λ, mhcd.dos.average_value), V_dl, ox; kT=kT)(E)
--- src/rate_constant.jl
+++ src/rate_constant.jl
@@ -5,62 +5,25 @@ using DelimitedFiles
 using BlackBoxOptim
 using Dierckx
 
+# TODO: think about reaction direction and quantum cap dispatches; there's probably a cleaner way to handle them...
 
+# for one direction
+function compute_k(E_min, E_max, V_app, model::KineticModel, ox::Bool; kT=.026) = quadgk(integrand(model, V_app, ox; kT=kT), E_min, E_max)[1] # return format is (value, error_bound)
 
-"""
-    compute_k(E_min, E_max, λ, V_dl, model)
-"""
-function compute_k(E_min, E_max, model::KineticModel, direction; kT=.026, C_q_calc=false, C_dl=10.0, Vq_min=-0.5, Vq_max=0.5)
-    @assert direction in ["red", "ox", "net"] "The supported reaction directions are red, ox, and net"
+# for net
+function compute_k(E_min, E_max, V_app, model::KineticModel; kT = .026) = quadgk(integrand(model, V_app; kT=kT), E_min, E_max)[1]
 
-    
+function compute_k(E_min, E_max, V_app, model::MarcusHushChidseyDOS, ox::Bool, C_dl=10.0, Vq_min=-0.5, Vq_max=0.5; kT = .026)
+    V_dl_interp = calculate_Vdl_interp(model.dos.interp_func, Vq_min, Vq_max, C_dl)
+    V_dl = V_dl_interp(V_app)
+    V_q = V_app - V_dl
+    quadgk(integrand(model, V_dl, ox; kT=kT, V_q=V_q), E_min, E_max)[1]
 end
 
-"""
-    compute_k_MHC(E_min, E_max, λ, V_dl, average_dos, ox=true; kT=.026)
-
-Compute the Marcus-Hush-Chidsey rate constant via GK quadrature by integrating the result of [`MHC_integrand`](@ref)(E, λ, V_dl, average_dos, ox; kT) from `E`=`E_min` to `E_max`.
-"""
-function compute_k_MHC(E_min, E_max, λ, V_dl, average_dos, ox; kT=.026)
-    fcn = E -> MHC_integrand(E, λ, V_dl, average_dos, ox; kT=.026)
-    quadgk(fcn, E_min, E_max)[1] # return format is (value, error_bound)
+# TODO: check that this gives the same as computing them separately and subtracting in all cases (including with V_q)
+function compute_k(E_min, E_max, V_app, model::MarcusHushChidseyDOS, C_dl=10.0, Vq_min=-0.5, Vq_max=0.5; kT = .026)
+    V_dl_interp = calculate_Vdl_interp(model.dos.interp_func, Vq_min, Vq_max, C_dl)
+    V_dl = V_dl_interp(V_app)
+    V_q = V_app - V_dl
+    quadgk(integrand(model, V_dl, ox; kT=kT, V_q=V_q), E_min, E_max)[1]
 end
-
-"""
-    compute_k_MHC_net(E_min, E_max, λ, V_dl, average_dos; kT=.026)
-
-Compute the net Marcus-Hush-Chidsey rate constant via GK quadrature by integrating the result of [`MHC_integrand_net`](@ref)(E, λ, V_dl, average_dos; kT) from `E`=`E_min` to `E_max`.
-"""
-function compute_k_MHC_net(E_min, E_max, λ, V_dl, average_dos; kT=.026)
-    fcn = E -> MHC_integrand_net(E, λ, V_dl, average_dos; kT=.026)
-    quadgk(fcn, E_min, E_max)[1] # return format is (value, error_bound)
-end
-
-
-function compute_k_MHC_DOS(E_min, E_max, λ, V_app, dos_func; kT=.026, C_q_calc=false, C_dl=10.0, Vq_min=-0.5, Vq_max=0.5)
-    if C_q_calc
-        ## calculate V_dl from V_app by calculating CQ
-        V_dl_interp = calculate_Vdl_interp(dos_func, Vq_min, Vq_max, C_dl)
-        V_dl = V_dl_interp(V_app)
-        V_q = V_app - V_dl
-        fcn = E->MHC_DOS_integrand_net(E, λ, V_dl, dos_func; kT=kT, V_q=V_q)
-    else
-        fcn = E->MHC_DOS_integrand_net(E, λ, V_app, dos_func; kT=kT, V_q=0)
-    end
-    quadgk(fcn, E_min, E_max)[1]
-end
-
-
-function compute_k_MHC_DOS_redox(E_min, E_max, λ, V_app, dos_func; ox=true, kT=.026, C_q_calc=false, C_dl=10.0, Vq_min=-0.5, Vq_max=0.5)
-    if C_q_calc
-        V_dl_interp = calculate_Vdl_interp(dos_func, Vq_min, Vq_max, C_dl)
-        V_dl = V_dl_interp(V_app)
-        V_q = V_app - V_dl
-        fcn = E->MHC_DOS_integrand(E, λ, V_dl, dos_func; ox=ox, kT=kT, V_q=V_q)
-    else
-        fcn = E->MHC_DOS_integrand(E, λ, V_dl, dos_func; ox=ox, kT=kT, V_q=0)
-    end
-    quadgk(fcn, E_min, E_max)[1]
-end
-
-

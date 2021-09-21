770f3dea9a03e63697677a4997d5ce0dbe12a3bf
Rachel Kurchin <rkurchin@cmu.edu>
2021-09-21 17:39:47 -0400

    typos
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- src/rate_constant.jl
+++ src/rate_constant.jl
@@ -12,8 +12,11 @@ using Dierckx
     compute_k(V_app, model::KineticModel; kwargs...)
     compute_k(E_min, E_max, V_app, model::IntegralModel, ox::Bool; kwargs...)
     compute_k(E_min, E_max, V_app, model::IntegralModel; kwargs...)
+    compute_k(E_min, E_max, V_app, model::MarcusHushChidseyDOS, calc_cq::Bool=false; C_dl = 10.0, Vq_min = -0.5, Vq_max = 0.5, kwargs...)
 
 Compute the rate constant k predicted by a given kinetic model at a applied voltage `V_app`. If a flag for reaction direction `ox` is supplied, `true` gives the oxidative and `false` the reductive direction, while omitting this flag will yield net reaction rate. If the model is an `IntegralModel`, integration bounds `E_min` and `E_max` must be supplied. Integration is done via GK quadrature.
+
+If calc_cq flag is passed, optionally compute voltage shifts due to quantum capacitance.
 """
 compute_k(V_app, model::KineticModel, ox::Bool; kwargs...) = model(V_app, ox; kwargs...)
 compute_k(E_min, E_max, V_app, model::IntegralModel, ox::Bool; kwargs...) =
@@ -23,17 +26,17 @@ compute_k(V_app, model::KineticModel; kwargs...) = model(V_app; kwargs...)
 compute_k(E_min, E_max, V_app, model::IntegralModel; kwargs...) =
     quadgk(integrand(model, V_app; kwargs...), E_min, E_max)[1]
 
-function compute_k(E_min, E_max, V_app, model::MarcusHushChidseyDOS, ox::Bool, calc_cq::Bool=false; C_dl = 10.0, Vq_min = -0.5, Vq_max = 0.5; kwargs...)
+function compute_k(E_min, E_max, V_app, model::MarcusHushChidseyDOS, ox::Bool; calc_cq::Bool=false, C_dl = 10.0, Vq_min = -0.5, Vq_max = 0.5, kwargs...)
     if calc_cq
-        compute_k_cq(E_min, E_max, V_app, model, ox; C_dl=C_dl, Vq_min=VQ_min, Vq_max=Vq_max; kwargs...)
+        compute_k_cq(E_min, E_max, V_app, model, ox; C_dl=C_dl, Vq_min=Vq_min, Vq_max=Vq_max, kwargs...)
     else
         compute_k(E_min, E_max, V_app, model, ox; kwargs...)
     end
 end
 
-function compute_k(E_min, E_max, V_app, model::MarcusHushChidseyDOS, calc_cq::Bool=false; C_dl = 10.0, Vq_min = -0.5, Vq_max = 0.5; kwargs...)
+function compute_k(E_min, E_max, V_app, model::MarcusHushChidseyDOS; calc_cq::Bool=false, C_dl = 10.0, Vq_min = -0.5, Vq_max = 0.5, kwargs...)
     if calc_cq
-        compute_k_cq(E_min, E_max, V_app, model, ox; C_dl=C_dl, Vq_min=VQ_min, Vq_max=Vq_max; kwargs...)
+        compute_k_cq(E_min, E_max, V_app, model, ox; C_dl=C_dl, Vq_min=VQ_min, Vq_max=Vq_max, kwargs...)
     else
         compute_k(E_min, E_max, V_app, model; kwargs...)
     end

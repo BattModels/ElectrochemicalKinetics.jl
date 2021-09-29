5dd4a6e89f1adaed29b3e4b1d13b986b1a34353a
Dhairya Gandhi <dhairya@juliacomputing.com>
2021-09-30 03:51:17 +0530

    add datastructures dep for adjoints

---
+++ src/datastructures.jl
@@ -0,0 +1,13 @@
+using DataStructures
+
+Zygote.@adjoint! function heappush!(xs, x, ord)
+  heappush!(xs, x, ord), Δ -> begin
+    (nothing, nothing, nothing)
+  end
+end
+
+Zygote.@adjoint! function heappop!(xs, ord)
+  heappop!(xs, ord), Δ -> begin
+    (nothing, nothing)
+  end
+end

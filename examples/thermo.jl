12c6f6903a0d3fb4dd9a857becb106aa3da965af
Dhairya Gandhi <dhairya@juliacomputing.com>
2021-10-02 06:11:29 +0530

    let vectors through

--- examples/thermo.jl
+++ examples/thermo.jl
@@ -11,10 +11,10 @@ muoB = 0.03
 Ω = 0.1
 
 # our familiar thermodynamic functions
-hs(x;Ω=Ω) = x*(1-x)*Ω # enthalpy of mixing
-s(x) = -kB*(x*log(x) + (1-x)*log(1-x)) # entropy per particle
-g_thermo(x; Ω=Ω, muoA=muoA, muoB=muoB, T=T) = hs(x;Ω=Ω) - T*s(x)+ muoA*(1-x) + muoB*x # Gibbs free energy per particle
-μ_thermo(x; Ω=Ω, muoA=muoA, muoB=muoB, T=T) = (1-2*x)*Ω + kB*T*log(x/(1-x)) + muoB-muoA # chemical potential
+hs(x;Ω=Ω) = @. x*(1-x)*Ω # enthalpy of mixing
+s(x) = @. -kB*(x*log(x) + (1-x)*log(1-x)) # entropy per particle
+g_thermo(x; Ω=Ω, muoA=muoA, muoB=muoB, T=T) = @. hs(x;Ω=Ω) - T*s(x)+ muoA*(1-x) + muoB*x # Gibbs free energy per particle
+μ_thermo(x; Ω=Ω, muoA=muoA, muoB=muoB, T=T) = @. (1-2*x)*Ω + kB*T*log(x/(1-x)) + muoB-muoA # chemical potential
 
 """
 µ and g with kinetic constributions, can be modeled using any <:KineticModel object
@@ -25,7 +25,7 @@ still being able to swap out model parameters by calling "function-builders" wit
 µ_kinetic(I, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T) = 
     x -> µ_thermo(x; Ω=Ω, muoA=muoA, muoB=muoB, T=T) + fit_overpotential((1-x)*km, I)
 g_kinetic(I, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T) = 
-    x -> g_thermo(x; Ω=Ω, muoA=muoA, muoB=muoB, T=T) + quadgk(x->fit_overpotential((1-x)*km, I), 0, x)[1]
+    x -> g_thermo(x; Ω=Ω, muoA=muoA, muoB=muoB, T=T) + quadgk(x->fit_overpotential((1 .- x)*km, I), 0., x)[1]
 
 # zeros of this function correspond to pairs of x's satisfying the common tangent condition for a given µ function
 function common_tangent(x, I, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T)
@@ -50,7 +50,7 @@ bv = ButlerVolmer(300)
 I_vals = 10 .^ (1.1:0.025:3.1)
 
 # this line takes a few seconds with B-V but aaages with anything else...
-pbs = [find_phase_boundaries(I, bv, T=330) for I in I_vals]
+pbs = find_phase_boundaries(I_vals, bv, T=330)
 
 plot(vcat(pbs...), hcat(I_vals, I_vals), label="phase boundary")
 xlabel!("x")

5328d46c020bccb9a4ea320c7811f39651fc5dff
Rachel Kurchin <rkurchin@cmu.edu>
2021-10-06 17:48:10 -0400

    vectorize over I
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- examples/thermo.jl
+++ examples/thermo.jl
@@ -22,17 +22,20 @@ muoB = 0.03
 These functions return single-argument functions (to easily use common-tangent function below while
 still being able to swap out model parameters by calling "function-builders" with different arguments).
 """
-µ_kinetic(I, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T) = 
+µ_kinetic(I::Real, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T) = 
     x -> µ_thermo(x; Ω=Ω, muoA=muoA, muoB=muoB, T=T) .+ [t[1] for t in fit_overpotential.((1 .-x).* repeat([km], length(x)), I)]
+
+µ_kinetic(I::Vector{<:Real}, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T) = µ_kinetic.(I, repeat([km], length(I)); Ω=Ω, muoA=muoA, muoB=muoB, T=T)
 # g_kinetic(I, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T) = 
 #     x -> g_thermo(x; Ω=Ω, muoA=muoA, muoB=muoB, T=T) .+ [q[1][1][1] for q in quadgk.(y->fit_overpotential.((1 .-y).* repeat([km], length(y)), I), 0, x)]
 
 # fully scalar version
-g_kinetic(I, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T) = 
+g_kinetic(I::Real, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T) = 
     x -> g_thermo(x; Ω=Ω, muoA=muoA, muoB=muoB, T=T) + quadgk(y->fit_overpotential((1 - y) * km, I), 0, x)[1][1]
+g_kinetic(I::Vector{<:Real}, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T) = g_kinetic.(I, repeat([km], length(I)); Ω=Ω, muoA=muoA, muoB=muoB, T=T)
 
 # for vector inputs x (for a vector of I's we would get a vector of functions that would then presumably each need to be broadcasted)
-function g_kinetic_vec(I, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T)
+function g_kinetic_vecx(I::Real, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T)
     function g_func(x::Vector{<:Real})
         @assert issorted(x) "Input x values must be sorted!"
         thermo_terms = g_thermo.(x; Ω=Ω, muoA=muoA, muoB=muoB, T=T)
@@ -45,31 +48,51 @@ function g_kinetic_vec(I, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T)
     return g_func
 end
 
+g_kinetic_vecxI(I::Vector{<:Real}, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T) = g_kinetic_vec.(I, repeat([km], length(I)); Ω=Ω, muoA=muoA, muoB=muoB, T=T)
+
 # zeros of this function correspond to pairs of x's satisfying the common tangent condition for a given µ function
 function common_tangent(x, I, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T)
-    g = g_kinetic(I, km; Ω=Ω, muoA=muoA, muoB=muoB, T=T)
+    g = g_kinetic_vec(I, km; Ω=Ω, muoA=muoA, muoB=muoB, T=T)
     µ = µ_kinetic(I, km; Ω=Ω, muoA=muoA, muoB=muoB, T=T)
     [((g(x[:,2]).-g(x[:,1]))./(x[:,2].-x[:,1]) .- μ(x[:,1]))[1], (μ(x[:,2]).-μ(x[:,1]))[1]]
 end
 
+# x should be N x 2, I of length N
+function common_tangent_vecI(x, I::Vector{<:Real}, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T)
+    gs = g_kinetic(I, km; Ω=Ω, muoA=muoA, muoB=muoB, T=T)
+    µs = µ_kinetic(I, km; Ω=Ω, muoA=muoA, muoB=muoB, T=T)
+    inds = 1:length(I)
+    dg = map(i -> gs[i](x[i, 2]) - gs[i](x[i, 1]), inds)
+    dx = x[:, 2] .- x[:, 1]
+    µ1 = map(i -> µs[i](x[i, 1])[1], inds)
+    µ2 = map(i -> µs[i](x[i, 2])[1], inds)
+    return hcat(dg ./ dx .- µ1, µ2 .- µ1) # result N x 2
+end
+
 function find_phase_boundaries(I, km::KineticModel; Ω=Ω, muoA=muoA, muoB=muoB, T=T)
     function myct!(storage, x)
-        res = common_tangent(x, I, km; Ω=Ω, muoA=muoA, muoB=muoB, T=T)
-        storage[1] = res[1]
-        storage[2] = res[2]
+        res = common_tangent_vecI(x, I, km; Ω=Ω, muoA=muoA, muoB=muoB, T=T) # N x 2
+        storage .= res
+        # for i in 1:length(I)
+        #     storage[i,1] = res[i,1]
+        #     storage[i,2] = res[i,2]
+        # end
     end
     # TODO: grad! here from AD
-    x1 = nlsolve(myct!, [0.05 0.95])
+    x1 = nlsolve(myct!, repeat([0.05 0.95], length(I)))
     x1.zero
 end
 
 
 bv = ButlerVolmer(300)
-I_vals = 10 .^ (1.1:0.025:3.1)
+# I_vals = 10 .^ (1.1:0.025:3.1)
+I_vals = 10 .^ (1.1:0.1:3.1)
 
 # this line takes a few seconds with B-V but aaages with anything else...
-pbs = [find_phase_boundaries(I, bv, T=330) for I in I_vals]
+# pbs = [find_phase_boundaries(I, bv, T=330) for I in I_vals]
+pbs = find_phase_boundaries(I_vals, bv, T=330)
 
-plot(vcat(pbs...), hcat(I_vals, I_vals), label="phase boundary")
+#plot(vcat(pbs...), hcat(I_vals, I_vals), label="phase boundary")
+plot(pbs, hcat(I_vals, I_vals), label="phase boundary")
 xlabel!("x")
 ylabel!("I")

9b028518b84526474a1c22e358efeab486b809da
Rachel Kurchin <rkurchin@cmu.edu>
2022-02-03 16:10:05 -0500

    add kT prefactor to fix mismatch between MHC and a-MHC, add in net dispatch, simplify some parentheses
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- src/kinetic_models.jl
+++ src/kinetic_models.jl
@@ -94,7 +94,7 @@ end
     AsymptoticMarcusHushChidsey(A, λ)
     AsymptoticMarchusHushChidsey(λ)
 
-Computes asymptotic solution to MHC model, as described in Zeng et al.: 10.1016/j.jelechem.2014.09.038d
+Computes asymptotic solution to MHC model, as described in Zeng et al.: 10.1016/j.jelechem.2014.09.038d, with a corrction prefactor of kT since there is an error in the nondimensionalization in that work.
 
 If initialized with one argument, assumes this to be the reorganization energy λ and sets the prefactor to 1.0.
 """
@@ -112,8 +112,19 @@ function (amhc::AsymptoticMarcusHushChidsey)(V_app, ox::Bool; kT::Real = 0.026)
     a = 1 + sqrt(λ_nondim)
     arg = (λ_nondim .- sqrt.(a .+ η.^2)) ./ (2 * sqrt(λ_nondim))
     pref = sqrt(π * λ_nondim) ./ (1 .+ exp.(-η))
-    return amhc.A .* pref .* erfc.(arg)
+    return kT * amhc.A .* pref .* erfc.(arg)
 end
+
+
+function (amhc::AsymptoticMarcusHushChidsey)(V_app; kT::Real = 0.026)
+    η = V_app ./ kT
+    λ_nondim = amhc.λ / kT
+    a = 1 + sqrt(λ_nondim)
+    arg = (λ_nondim .- sqrt.(a .+ η.^2)) ./ (2 * sqrt(λ_nondim))
+    pref = sqrt(π * λ_nondim) .* tanh.(η/2)
+    return kT * amhc.A .* pref .* erfc.(arg)
+end
+
 
 abstract type IntegralModel <: KineticModel end # "Marcus-like"
 
@@ -154,9 +165,9 @@ function integrand(mhc::MarcusHushChidsey, V_dl, ox::Bool; kT::Real = 0.026)
     function marcus_term(E)
         local exp_arg
         if ox
-            exp_arg = -( ((E .- (mhc.λ .- V_dl)) .^ 2)) ./ (4 * mhc.λ * kT)
+            exp_arg = -((E .- mhc.λ .+ V_dl) .^ 2) ./ (4 * mhc.λ * kT)
         else
-            exp_arg = -( ((E .- (mhc.λ .+ V_dl)) .^2)) ./ (4 * mhc.λ * kT)
+            exp_arg = -((E .- mhc.λ .- V_dl) .^2) ./ (4 * mhc.λ * kT)
         end
         exp.(exp_arg)
     end

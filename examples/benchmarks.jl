6fa3d82a672f38896470ed4c5ffc01f83a468fd5
Rachel Kurchin <rkurchin@cmu.edu>
2022-02-28 16:48:33 -0500

    add benchmarks
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

---
+++ examples/benchmarks.jl
@@ -0,0 +1,16 @@
+using ElectrochemicalKinetics
+using BenchmarkTools
+
+# reusing the ones from thermo_tests
+bv = ButlerVolmer(300)
+m = Marcus(5000, 0.3)
+amhc = AsymptoticMarcusHushChidsey(70000, 0.3)
+kms = [bv, m, amhc]
+
+### fit_overpotential
+k = 2e3
+
+# benchmarks, mean times
+@benchmark fit_overpotential($bv, $k) # ~16µs without AD, 500 with
+@benchmark fit_overpotential($m, $k) # ~30µs, 1.25 ms
+@benchmark fit_overpotential($amhc, $k) #~21µs, 690µs

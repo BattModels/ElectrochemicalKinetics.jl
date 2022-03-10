f2fb6103b3a84d7227b994f874a1fcc50ba12d47
Dhairya Gandhi <dhairya@juliacomputing.com>
2022-03-11 03:31:58 +0530

    simplification

--- src/integration_utils.jl
+++ src/integration_utils.jl
@@ -1,20 +1,19 @@
-using FastGaussQuadrature
+using FastGaussQuadrature: gausslegendre
 
 # helper function to be able to "naturally" use the interval of integration that we want with FastGaussQuadrature, which returns nodes and weights for the interval [-1, 1] (see https://en.wikipedia.org/wiki/Gaussian_quadrature#Change_of_interval)
 function setup_integration(f, N)
     nodes, weights = f(N)
-    function scale(lb::Real, ub::Real, nodes, weights)
+    function scale(lb::Real, ub::Real, nodes = nodes, weights = weights)
         # use nodes and weights here
         w = @. 0.5 * (ub - lb) * weights
         x = @. 0.5 * (ub + lb) + 0.5 * (ub - lb) * nodes
         x, w
     end
-    function scale(lb, ub)
+    function scale(lb, ub, nodes = nodes, weights = weights)
         ns_and_ws = scale.(lb, ub, Ref(nodes), Ref(weights))
         ns, ws = Zygote.unzip(ns_and_ws)
         reduce(hcat, ns), reduce(hcat, ws)
     end
-    scale(lb::Real, ub::Real) = scale(lb, ub, nodes, weights)
 end
 
 const scale = setup_integration(gausslegendre, 1000)

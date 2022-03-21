15d15f17d104d233c53d0e373403e19b3b473078
Dhairya Gandhi <dhairya@juliacomputing.com>
2022-03-21 06:36:48 +0530

    use EK.scale

--- test/thermo_tests.jl
+++ test/thermo_tests.jl
@@ -79,10 +79,6 @@ xs = [0.1, 0.5, 0.95]
     end
 end
 
-# Set up the integrator for `common_tangent` using defaults as in the thermo example
-unscaled_x, unscaled_w = quadfun(N)
-
-
 @testset "Kinetic g" begin
     g_200_T400_vals = Dict(
         ButlerVolmer => [0.0205857425, 0.037693617, 0.06661696], 
@@ -90,7 +86,7 @@ unscaled_x, unscaled_w = quadfun(N)
         AsymptoticMarcusHushChidsey => [0.0207838185, 0.0390031065, 0.0729910686]
         )
     g_50_2_vals = Dict(ButlerVolmer=>0.03519728018258, Marcus=>0.03395267141265, AsymptoticMarcusHushChidsey=>0.0347968044)
-    nodes_xs, weights_xs = ElectrochemicalKinetics.scale_integration_nodes(unscaled_x, unscaled_w, zero.(xs), xs)
+    nodes_xs, weights_xs = ElectrochemicalKinetics.scale(zero.(xs), xs)
     for km in kms
         @testset "$(typeof(km))" begin
             μ_50 = μ_kinetic(50, km)
@@ -117,18 +113,12 @@ end
     # simplest case, just one pair of x values (this function is still pretty slow though)
     v1 = find_phase_boundaries(100, km)
 
-    nodes, weights = ElectrochemicalKinetics.scale_integration_nodes(unscaled_x,     # nodes
-                            unscaled_w,     # weights
-                            zero.(v1),      # lb
-                            v1)             # ub
+    nodes, weights = ElectrochemicalKinetics.scale(zero.(v1), v1)
     common_tangent_def(args...; kwargs...) = common_tangent(args...; nodes = nodes, weights = weights, kwargs...)
 
     @test all(isapprox.(common_tangent_def(v1, 100, km), Ref(0.0), atol=1e-6))
     v2 = find_phase_boundaries(100, km, T=350)
-    nodes, weights = ElectrochemicalKinetics.scale_integration_nodes(unscaled_x,     # nodes
-                            unscaled_w,     # weights
-                            zero.(v2),      # lb
-                            v2)             # ub
+    nodes, weights = ElectrochemicalKinetics.scale(zero.(v2), v2)
     @test all(isapprox.(common_tangent_def(v2, 100, km, T=350), Ref(0.0), atol=1e-5))
     # they should get "narrower" with temperature
     @test v2[1] > v1[1]

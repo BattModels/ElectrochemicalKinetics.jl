ef88d7fb33f5ef8acb30de4a33bbdcb26383b115
Rachel Kurchin <rkurchin@cmu.edu>
2022-06-21 11:32:09 -0400

    fix sign convention in Marcus model so forward is positive net current
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- src/kinetic_models/Marcus.jl
+++ src/kinetic_models/Marcus.jl
@@ -14,7 +14,7 @@ end
 # default prefactor is 1
 Marcus(λ) = Marcus(1.0, λ)
 
-m_f((V, ps), ::Val{true}; kT = 0.026) = ps[1] .* exp.(-(ps[2].+ V).^2 ./ (4 .* ps[2] .* kT))
-m_f((V, ps), ::Val{false}; kT = 0.026) = ps[1] .* exp.(-(ps[2].- V).^2 ./ (4 .* ps[2] .* kT))
+m_f((V, ps), ::Val{true}; kT = 0.026) = ps[1] .* exp.(-(ps[2].- V).^2 ./ (4 .* ps[2] .* kT))
+m_f((V, ps), ::Val{false}; kT = 0.026) = ps[1] .* exp.(-(ps[2].+ V).^2 ./ (4 .* ps[2] .* kT))
 
 rate_f(::Marcus) = m_f

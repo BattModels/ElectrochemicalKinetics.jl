8f423ffed36693406a273104f6b4bd7e9ab90522
Rachel Kurchin <rkurchin@cmu.edu>
2022-07-07 15:49:53 -0400

    hopefully fix CI
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- test/phase_diagram_tests.jl
+++ test/phase_diagram_tests.jl
@@ -8,27 +8,27 @@ T = 298
     # enthalpy of mixing
     @test h(0.5) == 0.025 # value
     @test h(0.1) ≈ h(0.9) # symmetry
-    @test h([0.1, 0.2, 0.7]) ≈ [0.009, 0.016, 0.021] # vector input
+    @test all(h([0.1, 0.2, 0.7]) .≈ [0.009, 0.016, 0.021]) # vector input
     @test h(0.5; Ω=1) == 0.25 # kwargs
 
     # similar set for entropy...
-    @test isapprox(s(0.5), -kB * log(0.5), atol=1e-5)
+    @test isapprox(s(0.5f0), -kB * log(0.5f0), atol=1f-5)
     @test s(0.1) == s(0.9)
-    @test s([0.1, 0.2, 0.7]) ≈ [2.80134626433e-5, 4.3121323932e-5, 5.2640192129e-5]
+    @test all(s([0.1, 0.2, 0.7]) .≈ [2.8013f-5, 4.3121f-5, 5.264f-5])
 
     # ...and thermodynamic free energy...
-    @test g_thermo(0.5) == h(0.5) - T * s(0.5) + muoA * 0.5 + muoB * 0.5 ≈ 0.032200226968
+    @test g_thermo(0.5) == h(0.5) - T * s(0.5) + muoA * 0.5 + muoB * 0.5 ≈ 0.0322002f0
     @test isapprox(g_thermo(0.0), muoA, atol=1e-8)
     @test isapprox(g_thermo(1.0), muoB, atol=1e-8)
-    @test g_thermo([0.1, 0.2, 0.7]) ≈ [0.021651988, 0.02514984585595, 0.0323132232]
+    @test all(g_thermo([0.1, 0.2, 0.7]) .≈ [0.021651988f0, 0.02514984585595f0, 0.0323132232f0])
 
     # ...and thermodynamic chemical potential.
     @test all(isinf.(μ_thermo([0.0, 1.0])))
     @test μ_thermo(0.5) == μ_thermo(0.5, T=500) ≈ muoB - muoA
-    @test μ_thermo(0.1) ≈  0.0335760350387
-    @test μ_thermo(0.9, T=400) ≈ 0.0057368657199
-    @test μ_thermo([0.1, 0.2, 0.7]) ≈ [0.0335760350387, 0.034400441691438, -0.00824168486]
-    @test μ_thermo([0.1, 0.2, 0.7], T=350) ≈ [0.023730242495, 0.0281884382282, -0.004444931883]
+    @test μ_thermo(0.1) ≈ 0.033576035f0
+    @test μ_thermo(0.9, T=400) ≈ 0.00573686571f0
+    @test all(μ_thermo([0.1, 0.2, 0.7]) .≈ [0.033576f0, 0.03440044f0, -0.0082417f0])
+    @test all(μ_thermo([0.1, 0.2, 0.7], T=350) .≈ [0.02373024f0, 0.0281884f0, -0.00444493f0])
 end
 
 # prefactors are set so that k vs. η plots roughly line up for small η
@@ -43,27 +43,27 @@ xs = [0.1, 0.5, 0.95]
     # all these numbers are just references evaluated as of 2/24/22
     μ_50_vals = Dict(
         :ButlerVolmer=>Dict(
-            0.1 => 0.03838429151, 
-            0.5 => 0.01862765755, 
-            0.95 => 0.062373147), 
+            0.1 => 0.03838429f0, 
+            0.5 => 0.01862766f0, 
+            0.95 => 0.062373f0), 
         :Marcus=>Dict(
-            0.1 => 0.03874237118, 
-            0.5 => 0.01928206901, 
-            0.95 =>  0.0749319471),
+            0.1 => 0.03874237f0, 
+            0.5 => 0.019282f0, 
+            0.95 =>  0.074932f0),
         :AsymptoticMarcusHushChidsey=>Dict(
-            0.1 => 0.03890969477, 
-            0.5 => 0.01957132911, 
-            0.95 => 0.07351263138)
+            0.1 => 0.0389097f0, 
+            0.5 => 0.0195713f0, 
+            0.95 => 0.0735126f0)
         )
     μ_200_vals = Dict(
-        :ButlerVolmer=>[0.05242161033, 0.04250973994, 0.13059172875], 
-        :Marcus=>[0.0540075165676, 0.0458811724237, 0.212208797161],
-        :AsymptoticMarcusHushChidsey=> [0.0545105175689, 0.04634317606989, 0.174525852191]
+        :ButlerVolmer=>[0.0524216f0, 0.0425097f0, 0.1305917f0], 
+        :Marcus=>[0.0540075f0, 0.04588117f0, 0.212208797f0],
+        :AsymptoticMarcusHushChidsey=> [0.0545105f0, 0.046343176f0, 0.17452585f0]
         )
     μ_100_T400_vals = Dict(
-        :ButlerVolmer=>[0.0238392619734, 0.027028750035, 0.121278859754] , 
-        :Marcus=>[0.0245705001602, 0.02842981631828, 0.1529969664115],
-        :AsymptoticMarcusHushChidsey=>[0.02488745742498, 0.02890884116453, 0.14468534057]
+        :ButlerVolmer=>[0.02383926f0, 0.02702875f0, 0.12127886f0] , 
+        :Marcus=>[0.0245705f0, 0.028429816f0, 0.152996966f0],
+        :AsymptoticMarcusHushChidsey=>[0.0248874574f0, 0.028908841f0, 0.14468534]
         )
     for km in kms
         @testset "$(typeof(km))" begin
@@ -85,11 +85,11 @@ end
 
 @testset "Kinetic g" begin
     g_200_T400_vals = Dict(
-        :ButlerVolmer => [0.0205857425, 0.037693617, 0.06661696], 
-        :Marcus => [0.02073470111, 0.03874647481, 0.07399607032],
-        :AsymptoticMarcusHushChidsey => [0.0207838185, 0.0390031065, 0.0729910686]
+        :ButlerVolmer => [0.02058574, 0.0376936, 0.06661696], 
+        :Marcus => [0.0207347, 0.0387464748, 0.07399607],
+        :AsymptoticMarcusHushChidsey => [0.0207838, 0.0390031, 0.07299107]
         )
-    g_50_2_vals = Dict(:ButlerVolmer=>0.03519728018258, :Marcus=>0.03542169346909, :AsymptoticMarcusHushChidsey=>0.03552448666)
+    g_50_2_vals = Dict(:ButlerVolmer=>0.03519728018, :Marcus=>0.03542169347, :AsymptoticMarcusHushChidsey=>0.03552449)
     for km in kms
         @testset "$(typeof(km))" begin
             μ_50 = μ_kinetic(50, km)
@@ -128,9 +128,9 @@ end
     @test v3[2] < v1[2]
 
     # test actual numerical values too
-    @test isapprox(v1, [0.0458468, 0.839934], atol=1e-5)
-    @test isapprox(v2, [0.08379, 0.795023], atol=1e-5)
-    @test isapprox(v3, [0.1076, 0.675928], atol=1e-5)
+    @test all(isapprox.(v1, [0.0458468, 0.839934], atol=1e-5))
+    @test all(isapprox.(v2, [0.08379, 0.795023], atol=1e-5))
+    @test all(isapprox.(v3, [0.1076, 0.675928], atol=1e-5))
 
     # TODO: next for multiple currents at once (may require some syntax tweaks)
 

55eeed63c38e30e0c284090a16c1d087b35eddb2
longemen3000 <longemen3000@gmail.com>
2022-07-30 17:25:28 -0400

    add tests

--- src/kinetic_models/unitful.jl
+++ src/kinetic_models/unitful.jl
@@ -11,7 +11,7 @@ nounits_V(V::AbstractArray{E}) where E<:Real = V
 
 #transform to default units, if a correct unit is passed
 nounits_T(T::Unitful.Temperature) = Unitful.ustrip(Unitful.uconvert(u"K",T))
-nounits_V(V::Unitful.Voltage) = Unitful.ustrip(Unitful.uconvert(u"V",T))
+nounits_V(V::Unitful.Voltage) = Unitful.ustrip(Unitful.uconvert(u"V",V))
 
 #if an array of units is passed, transform to unitless array
 nounits_T(T::AbstractArray{E}) where E<:Unitful.Temperature = nounits_T.(T)
